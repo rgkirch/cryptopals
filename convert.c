4f8e77dc921984db739a24d0a5af718bbb153582
richie <rgkirch@gmail.com>
2017-03-09 13:52:35 -0500

    base 64 convert woks and have test for it

--- convert.c
+++ convert.c
@@ -1,35 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int indexOf(char c, char* cs, int length);
+int indexOf(unsigned char c, unsigned char* cs, int length);
 
 //convert hex to base 64
-char* convertFromHex(char* hex, int length)
+void convertFromHex(unsigned char* b, unsigned char* hex, int length)
 {
     int i = 0;
-    char c[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-    char* b = (char*) malloc(length * sizeof(char) / 2);
+    unsigned char c[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
     while(i < length)
     {
-        char high = indexOf(hex[i], c, sizeof(c) / sizeof(char));
-        char low = indexOf(hex[i+1], c, sizeof(c) / sizeof(char));
-        b[i / 2] = (high << 4) | low;
-        i += 2;
+        unsigned char high = indexOf(hex[i*2], c, sizeof(c) / sizeof(unsigned char));
+        unsigned char low = indexOf(hex[i*2+1], c, sizeof(c) / sizeof(unsigned char));
+        b[i] = (high << 4) | low;
+        i++;
     }
-    return b;
 }
 
-int indexOf(char c, char* cs, int length)
+int indexOf(unsigned char c, unsigned char* cs, int length)
 {
     if(length < 1) { return -1; }
     if(cs == NULL) { return -1; }
-    char* ptr = cs;
-    char* end = cs + length;
-    printf("%p\n", cs);
-    printf("%p\n", end);
-    while(ptr++ != end)
+    unsigned char* ptr = cs;
+    unsigned char* end = cs + length;
+//    printf("%p\n", cs);
+//    printf("%p\n", end);
+    while(ptr != end)
     {
         if(*ptr == c) return ptr - cs;
+        ptr++;
     }
     return -1;
 }
--- testConvert.c
+++ testConvert.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 #include <assert.h>
 
-extern char* convertFromHex(char* hex, int length);
+convertFromHex(unsigned char* out, unsigned char* hex, int length);
 
-int equals(char* a, char* b, int len)
+int equals(unsigned char* a, unsigned char* b, int len)
 {
     for(int i = 0; i < len; i++)
     {
@@ -18,26 +18,26 @@ int equals(char* a, char* b, int len)
 
 void simpleTest()
 {
-    char hex[] = {"41"};
-    char *bytes = convertFromHex(hex, 2);
+    unsigned char hex[] = {"41"};
+    unsigned char bytes[sizeof(hex)];
+    convertFromHex(bytes, hex, 2);
     assert(bytes[0] == 'A');
-    free(bytes);
 }
 void allTest()
 {
     int num = 256;
-//    char* buffer = (char*)malloc(256);
-//    char* hexInput = (char*)malloc(256 * 2);
-    char buffer[num + 1]; // fill it with binary shit
-    char hexInput[num*2]; // the hex thet represents the binary shit
-    char* hexOutput; // go back to the binary shit
+//    unsigned char* buffer = (unsigned char*)malloc(256);
+//    unsigned char* hexInput = (unsigned char*)malloc(256 * 2);
+    unsigned char buffer[num]; // fill it with binary shit
+    unsigned char hexInput[num*2]; // the hex thet represents the binary shit
+    unsigned char result[num]; // go back to the binary shit
     for(int i = 0; i < num; i++)
     {
         buffer[i] = i;
     }
     for (int i = 0; i < num; i++)
     {
-        char t[3];
+        unsigned char t[3];
         int len = snprintf(NULL, 0, "%x", buffer[i]);
         snprintf(t, 3, "%x", buffer[i]);
         if (len == 1)
@@ -49,24 +49,23 @@ void allTest()
             hexInput[i*2+1] = t[1];
         }
     }
-    printf("%s\n", hexInput);
-    hexOutput = convertFromHex(hexInput, 256);
-    assert(equals(hexOutput, hexInput, 256));
-    free(hexOutput);
+    convertFromHex(result, hexInput, 256);
+
+    assert(equals(result, buffer, 256));
 }
 
 void longTest()
 {
-    char hex[] = {"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"};
-    char *bytes = convertFromHex(hex, sizeof(hex));
-    for(int i = 0; i < sizeof(hex); ++i)
+    unsigned char hex[] = {"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"};
+    unsigned char bytes[sizeof(hex) / 2];
+    convertFromHex(bytes, hex, sizeof(bytes));
+    for(int i = 0; i < sizeof(bytes); ++i)
     {
         printf("%c", bytes[i]);
     }
     printf("\n");
-//    char answer[] = {};
+//    unsigned char answer[] = {};
 //    assert(equals(hex, answer, sizeof(hex)));
-    free(bytes);
 }
 
 typedef void(*test)();
@@ -74,10 +73,10 @@ test tests[] = {simpleTest, longTest, allTest};
 
 int main()
 {
-//    for(int i = 0; i < sizeof(tests) / sizeof(test); i++)
-//    {
-//        tests[i]();
-//    }
-    allTest();
+    for(int i = 0; i < sizeof(tests) / sizeof(test); i++)
+    {
+        tests[i]();
+    }
+//    allTest();
 //    assert(4 << 4 | 1 == 'A');
 }
