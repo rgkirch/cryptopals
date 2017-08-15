3a177cc6bca1df1a9e2ab9b6fd54ed3c81c4aa50
Richie Kirchofer <rgkirch@gmail.com>
2017-08-14 22:04:04 -0400

    umm, how to make this deduce

--- src/challenge03.h
+++ src/challenge03.h
@@ -7,50 +7,56 @@
 
 using namespace std;
 
-vector<float> get_letter_frequencies(string filename) {
-    struct Sum {
-        Sum() : count(0) {};
-        operator()(int num) { count += num; }
-        int count;
-    };
-    auto maybe_string = read_file_if_exists(filename);
-    if(maybe_string) {
-        vectorc<char> letter_counts = count_letters(maybe_string.get());
-        vector<float> letter_frequency;
-        letter_frequency.reserve(256);
-        int total_number_of_chars = for_each(begin(letter_counts), end(letter_counts), Sum()).count;
-        transform(begin(letter_counts), end(letter_counts), begin(letter_frequency), [=](int count)->float{ return count / (float)total_number_of_chars; });
+boost::optional<string> read_file_if_exists(string filename) {
+    string data;
+    if (boost::filesystem::exists(filename)) {
+        fstream file(filename, ios_base::in);
+        char c;
+        while (file >> c) {
+            data += c;
+        }
+        return data;
+    } else {
+        return {};
     }
-    return letter_frequency;
 }
 
-vector<char> count_letters(string data) {
-    vector<char> data;
+vector<int> count_letters(string s) {
+    vector<int> data;
     data.resize(256);
-    for (auto c : data) {
+    for (auto c : s) {
         data[c]++;
     }
     return data;
 }
 
-boost::optional<string> read_file_if_exists(string filename) {
-    string data;
-    if (boost::filesystem::exists(filename)) {
-        try {
-            fstream file(filename, ios_base::in);
-            char c;
-            while(file >> c) {
-                data.append(c);
-            }
-            return data;
-        } catch (exception e) {
-            cerr << e.what() << endl;
-            return {};
-        }
-
+template <typename A, typename B, typename F>
+auto myBind(boost::optional<B> b, F f)->boost::optional<result_of_t<F(B)>> {
+//    static_assert(std::result_of_t<F(*B)> == boost::optional, "");
+    if(b) {
+        return f(*b);
     } else {
         return {};
     }
+}
+
+vector<float> get_letter_frequencies(string filename) {
+    struct Sum {
+        Sum() : count(0) {};
+        int operator()(int num) { return count += num; }
+        operator int() { return count; }
+        int count;
+    };
+    return myBind(read_file_if_exists(filename), [](auto str)->vector<float> {
+        vector<int> letter_counts = count_letters(str);
+        vector<float> letter_frequency;
+        letter_frequency.reserve(256);
+        int total_number_of_chars = for_each(begin(letter_counts), end(letter_counts), Sum());
+        transform(begin(letter_counts), end(letter_counts), begin(letter_frequency),
+                  [=](int count) -> float { return count / (float) total_number_of_chars; });
+
+        return letter_frequency;
+    });
 }
 
 
